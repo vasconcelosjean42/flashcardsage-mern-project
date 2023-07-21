fd5d2fa556f78c6f008e10100bb7bc40ce34a4e2
sti <sti@matera.com>
2023-07-21 15:57:06 -0300

    retactoring the code - added the folder controllers and pass throw endpoint to this folder, making the code more clean

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,38 +6,19 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 
 import Deck from './models/Deck';
+import decksRouter from './controllers/decks';
 
 const PORT = 5001
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+
 app.get('/', (req: Request, res: Response) => {
   res.send('hello world')
 })
 
-app.get('/decks', async (req: Request, res: Response) => {
-  const decks = await Deck.find({})
-  res.json(decks)
-})
-
-app.post('/decks', async (req: Request, res: Response) => {
-  const body = req.body
-  console.log(req.body)
-  
-  const newDeck = new Deck({
-    title: body.title
-  })
-  
-  const createdDeck = await newDeck.save()
-  res.json(createdDeck)
-})
-
-app.delete('/decks/:id', async (req: Request, res: Response) => {
-  const {id} = req.params
-  const deck = await Deck.findByIdAndDelete(id)
-  res.json(deck)
-})
+app.use('/decks', decksRouter)
 
 const db = mongoose.connect(process.env.MONGO_URL!).then(() => {
     console.log(`litening to the port ${PORT}`)
